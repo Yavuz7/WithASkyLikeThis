28e7c4b62fe103c6f65b047ca60b06dd13fdba42
Yavuz7 <yavuzyurtseven1@gmail.com>
2024-05-03 12:44:59 -0400

    Initial Commit

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import Footer from "./src/components/layout/Footer";
+import Header from "./src/components/layout/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.styles.css";
+
+export default function Footer() {
+  return (
+    <footer>
+      <p>
+        “Every day may not be good, but there`s something good in every day” -
+        Alice Morse Earle.
+      </p>
+    </footer>
+  );
+}
